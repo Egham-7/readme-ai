000476c3f369deab709106fcf2b1b0cf0c1aa375
botirk38 <botir.khaltaev@squaredup.com>
2025-01-31 22:14:04 +0000

    Feat: Add tanstack query

--- readme-ai-frontend/src/main.tsx
+++ readme-ai-frontend/src/main.tsx
@@ -2,15 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
 
+const queryClient = new QueryClient();
+
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <RouterProvider router={routeTree} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={routeTree} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
