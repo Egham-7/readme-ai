ab2f8405eb16cc01c76be0072fa93b4525f43e91
botirk38 <botir.khaltaev@squaredup.com>
2025-02-12 15:47:00 +0000

    Feat: Allow all origins~

--- readme_ai_backend/readme_ai/main.py
+++ readme_ai_backend/readme_ai/main.py
@@ -59,7 +59,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.APP_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
